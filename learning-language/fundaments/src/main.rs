d6d84d836ea14ecde8a2b690e98f3546410aa7fb
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-01 16:40:02 -0500

    Feature: Fundaments of language

--- learning-language/fundaments/src/main.rs
+++ learning-language/fundaments/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Mi primera APP en Rust");
+    // Learning the fundamentos of Rust
+    // Print by console, use of mutable and unmutable vars and const.
+    fundaments();
+
+
+}
+
+fn fundaments() {
+    println!("My first Rust program!");
+    let mut x = 10;
+
+    println!("X Value: {}", x);
+
+    x = 31;
+
+    println!("X value: {}", x);
+
+    let x = "Hi there, my name is Juan Francisco";
+    println!("X Value: {}", x);
 }
