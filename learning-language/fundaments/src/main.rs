a26323dbd8cfdb3964eac6c4b2f3cd7afd8875ca
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-01 16:32:24 -0500

    Feature: Creation first project on Rust

---
+++ learning-language/fundaments/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Mi primera APP en Rust");
+}
---
+++ learning-language/fundaments/target/debug/fundaments.d
@@ -0,0 +1,1 @@
+E:\Proyectos\ GH\rust-learning-lng\learning-language\fundaments\target\debug\fundaments.exe: E:\Proyectos\ GH\rust-learning-lng\learning-language\fundaments\src\main.rs
